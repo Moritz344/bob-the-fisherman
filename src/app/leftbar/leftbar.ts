af155a7bd270a81d4e03f128fd83fe0b17ab565b
Moritz344 <moritzmaier34@gmail.com>
2026-05-17 19:40:04 +0200

    add leftbar component

---
+++ src/app/leftbar/leftbar.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-leftbar',
+  imports: [],
+  templateUrl: './leftbar.html',
+  styleUrl: './leftbar.css',
+})
+export class Leftbar {
+
+}
