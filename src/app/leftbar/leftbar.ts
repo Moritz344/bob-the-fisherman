d350ba6c86a37210c2f8bc1340fd66b2712bccbe
Moritz344 <moritzmaier34@gmail.com>
2026-05-19 20:38:44 +0200

    create leftbar component

--- src/app/leftbar/leftbar.ts
+++ src/app/leftbar/leftbar.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,signal,OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-leftbar',
@@ -6,6 +6,16 @@ import { Component } from '@angular/core';
   templateUrl: './leftbar.html',
   styleUrl: './leftbar.css',
 })
-export class Leftbar {
+export class Leftbar implements OnInit {
+  public currentSelectedTab = signal<string>("");
+
+  constructor() {}
+
+  ngOnInit(): void {
+  }
+
+  onTab(tab: string) {
+    this.currentSelectedTab.set(tab);
+  }
 
 }
