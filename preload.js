3488cc9254e72a2deeb8f9e2065ad0ab1baddbe1
Moritz344 <moritzmaier34@gmail.com>
2026-05-19 20:38:21 +0200

    start,stop and get tested version list functions

--- preload.js
+++ preload.js
@@ -1,5 +1,8 @@
 const { contextBridge, ipcRenderer } = require("electron");
 
 contextBridge.exposeInMainWorld("electronAPI", {
+    getMinecraftVersions: () => ipcRenderer.invoke("get-minecraft-versions"),
+  startBot: (host,port,version,auth,username) => ipcRenderer.invoke("start-bot", host,port,version,auth,username),
 
+  stopBot: () => ipcRenderer.invoke("stop-bot")
 });
