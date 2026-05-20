5c6271e055cf8bbfedcea5797284b827469133e8
Moritz344 <moritzmaier34@gmail.com>
2026-05-20 21:16:16 +0200

    add settings route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
+import { Settings } from './settings/settings';
 
 export const routes: Routes = [
-  { path: "",component: Home}
+  { path: "",component: Home},
+  { path: "settings",component: Settings}
 ];
