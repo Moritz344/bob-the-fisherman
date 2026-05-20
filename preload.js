b79c757c735fc564e30e4a27d191506b893eccc1
Moritz344 <moritzmaier34@gmail.com>
2026-05-20 21:16:05 +0200

    add game-logs event

--- preload.js
+++ preload.js
@@ -1,8 +1,8 @@
 const { contextBridge, ipcRenderer } = require("electron");
 
 contextBridge.exposeInMainWorld("electronAPI", {
-    getMinecraftVersions: () => ipcRenderer.invoke("get-minecraft-versions"),
+  getMinecraftVersions: () => ipcRenderer.invoke("get-minecraft-versions"),
   startBot: (host,port,version,auth,username) => ipcRenderer.invoke("start-bot", host,port,version,auth,username),
-
-  stopBot: () => ipcRenderer.invoke("stop-bot")
+  stopBot: () => ipcRenderer.invoke("stop-bot"),
+  gameLogs: (callback) => ipcRenderer.on("game-logs", (_event, data) => callback(data)),
 });
