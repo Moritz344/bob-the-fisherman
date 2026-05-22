b053998fcdaf98ac869f8bb025e008fbbb296c98
Moritz344 <moritzmaier34@gmail.com>
2026-05-22 18:16:52 +0200

    add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
 import { Settings } from './settings/settings';
+import { Loot } from './loot/loot';
 
 export const routes: Routes = [
   { path: "",component: Home},
-  { path: "settings",component: Settings}
+  { path: "settings",component: Settings},
+  { path: "loot",component: Loot}
 ];
