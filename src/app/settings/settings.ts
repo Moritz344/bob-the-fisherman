a7901cc591a4f7934aa4035b61ddd0c92ef7a942
Moritz344 <moritzmaier34@gmail.com>
2026-05-22 18:18:23 +0200

    give version options a value/remvoe interface/load settings on init

--- src/app/settings/settings.ts
+++ src/app/settings/settings.ts
@@ -1,16 +1,9 @@
-import { Component,signal,inject } from '@angular/core';
+import { Component,signal,inject,OnInit } from '@angular/core';
 import { Leftbar } from '../leftbar/leftbar';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { SettingsService } from '../settings-service';
-
-interface currentSelectedType{
-  host: string,
-  port: number,
-  username: string,
-  version: string,
-  auth: string
-}
+import { currentSelectedType } from '../models/current.model';
 
 @Component({
   selector: 'app-settings',
@@ -18,24 +11,37 @@ interface currentSelectedType{
   templateUrl: './settings.html',
   styleUrl: './settings.css',
 })
-export class Settings {
+export class Settings implements OnInit{
   public settings = inject(SettingsService);
-  public currentSelected = signal<currentSelectedType>({port: 0,username: "",version: "",auth: "",host: ""});
+  public currentSelected = signal<currentSelectedType>({port: 3000,username: "Bob",version: "1.21.11",auth: "offline",host: "localhost"});
   public versionData = signal([]);
 
   constructor() {
-    this.initCurrentSelected();
     this.initVersions();
+  }
+
+  ngOnInit(): void {
+    this.initCurrentSelected();
   }
 
   initCurrentSelected() {
     this.currentSelected.set({
-      host: "localhost",
-      auth: "offline",
-      version: "1.21.11",
-      username: "Bob",
-      port: 3000
+      host: this.settings.getSettings().host,
+      auth: this.settings.getSettings().auth,
+      version: this.settings.getSettings().version,
+      username: this.settings.getSettings().username,
+      port: this.settings.getSettings().port,
+      started: this.settings.getStarted()
     });
+    console.log("settings got:",this.currentSelected());
+  }
+
+  onChooseMode() {
+    console.log(this.currentSelected().auth);
+    // 46685
+
+
+
   }
 
   async initVersions() {
