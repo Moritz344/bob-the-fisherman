ff45341c054236a68aa48b862ca4f226285e3bd5
Moritz344 <moritzmaier34@gmail.com>
2026-05-22 18:17:35 +0200

    load started status and other settings on init

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -10,7 +10,8 @@ interface currentSelectedType{
   port: number,
   username: string,
   version: string,
-  auth: string
+  auth: string,
+  started: boolean
 }
 
 @Component({
@@ -23,7 +24,7 @@ export class Home implements OnInit {
   public settings = inject(SettingsService);
 
   public started = signal<boolean>(false);
-  public currentSelected = signal<currentSelectedType>({port: 0,username: "",version: "",auth: "",host: ""});
+  public currentSelected = signal<currentSelectedType>({port: 0,username: "",version: "",auth: "",host: "",started: false});
   public versionData = signal([]);
   // 36147
 
@@ -38,11 +39,20 @@ export class Home implements OnInit {
     } else {
       this.settings.stopBot();
     }
+    this.settings.setStarted(this.started());
   }
 
 
   ngOnInit(): void {
-    this.currentSelected.set(this.settings.getSettings());
+    this.currentSelected.set(({
+      auth: this.settings.getSettings().auth,
+      username: this.settings.getSettings().username,
+      host: this.settings.getSettings().host,
+      version: this.settings.getSettings().version,
+      port: this.settings.getSettings().port,
+      started: this.settings.getStarted()
+    }))
+    this.started.set(this.currentSelected().started);
     console.log("got",this.currentSelected());
   }
 
