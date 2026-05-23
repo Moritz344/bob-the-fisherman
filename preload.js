a94d64bf2826ca7c4e8aa944fb64ecf1a80a18ae
Moritz344 <moritzmaier34@gmail.com>
2026-05-23 19:10:29 +0200

    new ipc

--- preload.js
+++ preload.js
@@ -3,6 +3,10 @@ const { contextBridge, ipcRenderer } = require("electron");
 contextBridge.exposeInMainWorld("electronAPI", {
   getMinecraftVersions: () => ipcRenderer.invoke("get-minecraft-versions"),
   startBot: (host,port,version,auth,username) => ipcRenderer.invoke("start-bot", host,port,version,auth,username),
+  getBotSettings: () => ipcRenderer.invoke("get-bot-settings"),
+  saveBotSettings: (data) => ipcRenderer.invoke("save-settings",data),
   stopBot: () => ipcRenderer.invoke("stop-bot"),
   gameLogs: (callback) => ipcRenderer.on("game-logs", (_event, data) => callback(data)),
+  botError: (callback) => ipcRenderer.on("bot-error", (_event, msg) => callback(msg)),
+  showError: (title,msg) => ipcRenderer.invoke("show-error",title,msg),
 });
