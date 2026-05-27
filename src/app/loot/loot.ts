eae9165369d6ae041fb3175a4e0b2b2b8bf1f9d7
Moritz344 <moritzmaier34@gmail.com>
2026-05-27 21:17:52 +0200

    add loot component

--- src/app/loot/loot.ts
+++ src/app/loot/loot.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component,inject } from '@angular/core';
+import { Leftbar } from '../leftbar/leftbar';
+import { SettingsService } from '../settings-service';
 
 @Component({
   selector: 'app-loot',
-  imports: [],
+  imports: [Leftbar],
   templateUrl: './loot.html',
   styleUrl: './loot.css',
 })
 export class Loot {
+  public settings = inject(SettingsService);
+  public items = this.settings.caughtItems;
+
+  constructor() {}
 
 }
