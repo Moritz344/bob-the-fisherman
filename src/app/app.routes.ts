2cd1f2d57fdc32823d01dce62357e4e97d3d7505
Moritz344 <moritzmaier34@gmail.com>
2026-05-30 00:20:16 +0200

    add log route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { Home } from './home/home';
 import { Settings } from './settings/settings';
 import { Loot } from './loot/loot';
+import { Log } from './log/log';
 
 export const routes: Routes = [
   { path: "",component: Home},
   { path: "settings",component: Settings},
+  { path: "log",component: Log},
   { path: "loot",component: Loot}
 ];
