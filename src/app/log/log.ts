601f3af5b2c108f5cd864fe04453ff08f2ff3974
Moritz344 <moritzmaier34@gmail.com>
2026-05-31 21:22:02 +0200

    remove leftbar import

--- src/app/log/log.ts
+++ src/app/log/log.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { Leftbar } from '../leftbar/leftbar';
+import { Topbar } from '../topbar/topbar';
 import { ChatLog } from '../chat-log/chat-log';
 
 // TODO: Search for logs
 
 @Component({
   selector: 'app-log',
-  imports: [Leftbar,ChatLog],
+  imports: [Topbar,ChatLog],
   templateUrl: './log.html',
   styleUrl: './log.css',
 })
