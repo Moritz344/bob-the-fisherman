8eb6d4c9be073c8916c0102e52b4bfd5cb8fc70c
Moritz344 <moritzmaier34@gmail.com>
2026-05-31 12:57:24 +0200

    add view route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { Home } from './home/home';
 import { Settings } from './settings/settings';
 import { Loot } from './loot/loot';
 import { Log } from './log/log';
+import { WorldView } from './world-view/world-view';
 
 export const routes: Routes = [
   { path: "",component: Home},
   { path: "settings",component: Settings},
   { path: "log",component: Log},
-  { path: "loot",component: Loot}
+  { path: "loot",component: Loot},
+  { path: "view",component: WorldView},
 ];
