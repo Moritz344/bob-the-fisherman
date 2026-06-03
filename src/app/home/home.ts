7fae8e41ad24ac0b551a2510206998c0c2965d77
Moritz344 <moritzmaier34@gmail.com>
2026-06-03 20:55:53 +0200

    remove task selection/handle commands/

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -25,7 +25,7 @@ export class Home implements OnInit {
   public isFollowingPlayer = signal<boolean>(false);
   public isLookingForWater = signal<boolean>(false);
 
-  public currentMode = signal<string>("");
+  public currentBotTask = this.settings.currentTask;
 
   constructor() {
     this.initBotSettings();
@@ -53,49 +53,72 @@ export class Home implements OnInit {
     }
   }
 
-  async onStartFishing() {
-    if (!this.isFishing()) {
-      this.settings.startFishing();
-      console.log("fish!");
+  onCommand(command: string) {
+      console.log("COMMAND:",command);
+    if (command == "start" ) {
+      this.onStartFishing();
+    } else if (command == "stop") {
+      this.onStopFollowingPlayer();
+      this.onStopFishing();
+    } else if (command.includes("follow")) {
+      let playerToFollow = command.split(" ")[1];
+      console.log(playerToFollow);
+      if (playerToFollow == '' || !playerToFollow) {
+        playerToFollow = this.currentSelectedActionSettings().playerToFollow;
+      }
+      this.onFollowPlayer(playerToFollow);
     }
-    this.isFishing.update( (x: boolean) => !x);
+  }
+
+  onStopFishing() {
+    if (!this.isFishing()) {
+      return;
+    }
+    this.isFollowingPlayer.set(false);
+    this.isLookingForWater.set(false);
+
+    this.settings.stopFishing();
+    this.isFishing.set(false);
+  }
+
+  async onStartFishing() {
+    this.isFollowingPlayer.set(false);
+    this.isLookingForWater.set(false);
+
+    await this.settings.startFishing();
+
+    this.isFishing.set(true);
   }
 
   async onFindWater() {
+    this.isFollowingPlayer.set(false);
+    this.isFishing.set(false);
+
+    await this.settings.findWater();
     this.isLookingForWater.update( (x: boolean) => !x);
-    if (!this.isLookingForWater()) {
-
-    }
-
   }
 
-  onSelectMode(mode: string) {
-    this.currentMode.set(mode);
-    console.log(this.currentMode());
-
-    switch (this.currentMode()) {
-      case "fishing":
-        this.onStartFishing()
-        break;
-      case "find_water":
-        break;
-      case "follow_player":
-        break;
-      default:
-        break;
-    }
-
-  }
-
-  async onFollowPlayer() {
-    this.isFollowingPlayer.update( (x: boolean) => !x);
+  onStopFollowingPlayer() {
     if (!this.isFollowingPlayer()) {
-
+      return;
     }
+    this.isLookingForWater.set(false);
+    this.isFishing.set(false);
 
+    this.settings.stopFollowingPlayer();
+    this.isFollowingPlayer.set(false);
   }
 
+  async onFollowPlayer(name: string) {
+    this.isLookingForWater.set(false);
+    this.isFishing.set(false);
 
+    if (!this.isFollowingPlayer()) {
+      await this.settings.followPlayer(name);
+    }
+    this.isFollowingPlayer.set(true);
+
+  }
   ngOnInit(): void {
     console.log("current state",this.currentSelected().started);
     this.started.set(this.started());
