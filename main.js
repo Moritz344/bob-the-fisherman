3bf09c6f7c1a7b903d525a70a0bb7d3aef78b5c9
Moritz344 <moritzmaier34@gmail.com>
2026-06-07 16:37:10 +0200

    use bot-engine functions to avoid duplication

--- main.js
+++ main.js
@@ -5,17 +5,14 @@ const {
   ipcMain,
   dialog
 } = require("electron");
-
-
-// FIXME: load them if just in time to improve perfomance
-const mineflayer = require('mineflayer')
-const Movements = require('mineflayer-pathfinder').Movements;
-const { GoalFollow } = require('mineflayer-pathfinder').goals;
+const engine = require("./bot-engine.cjs");
+const mineflayer = require('mineflayer');
 
 let bot;
-let isFishing = false;
 let win;
 let store;
+
+const botStartCooldown = 2000;
 
 function stopBot() {
   bot.quit();
@@ -43,10 +40,6 @@ async function getItemImage(name) {
 
 }
 
-function getLogTime() {
-    const date = new Date();
-    return date.getHours().toString().padStart(2,"0") + ":" + date.getMinutes().toString().padStart(2,"0") + ":" + date.getSeconds().toString().padStart(2,"0") ;
-}
 
 
 async function initBot(auth,host, port,username,version) {
@@ -54,7 +47,7 @@ async function initBot(auth,host, port,username,version) {
 
       console.log("auth:",auth);
       console.log("port",port);
-      win.webContents.send("game-logs",  getLogTime() + " Creating Bot...");
+      win.webContents.send("game-logs",  engine.getLogTime() + " Creating Bot...");
       bot = mineflayer.createBot({
         host,
         ...(port ? { port: Number(port) } : {}),
@@ -67,59 +60,54 @@ async function initBot(auth,host, port,username,version) {
             authTitle: 'Microsoft Login Required',
             message: data.message
           })
-        }
+        },
+        hideErrors: true
       });
 
 
+    const mcData = require('minecraft-data')(bot.version);
+    engine.setBot(bot, mcData);
+    engine.setLogFn((msg) => {
+      win.webContents.send("game-logs", engine.getLogTime() + " " + msg);
+    });
 
     bot.once('spawn', async() => {
-      console.log("Bot spawned");
-      win.webContents.send("game-logs",  getLogTime() + " Bot spawned");
-      checkForFishingRoadInInventory();
-      //await pathFindToWater();
-      // FIXME: this works because the world loads in this time i think needs better fix
-      setTimeout( async() => {
-        await startFishing();
-      },3000);
+      win.webContents.send("game-logs",  engine.getLogTime() + " Bot spawned");
+      setTimeout( () => {
+        engine.setBotReady(true);
+      },botStartCooldown);
 
     });
 
-    //bot.on("kicked",(reason,loggedIn) => {
-    //  win.webContents.send("bot-error","Bot Kicked!");
-    //});
-
-    bot.on("end",(reason) => {
-      console.log("Bot ended reason: ", reason);
-      if (reason == "socketClosed") {
-        win.webContents.send("bot-error","Bot crashed! Please make sure the settings for the bot are correct." + "\n Settings: \nHost:  " + host + "\nPort: " + port + "\n" + "auth: " + auth + "\n" + "name:  " + username + "\n" + "version:  " + version);
-      }
+    bot.on("error",(err) => {
+      const errorMessage = engine.error(err.code,{ host,port});
+      win.webContents.send("bot-error",errorMessage);
+    })
+    bot.on("end",() => {
+      win.webContents.send("game-log","Bot stopped");
     })
 
 
     bot.on('whisper', async(username, message) => {
       if (username === bot.username) return
       if (message == "!start") {
-        startFishing();
+        engine.startFishing();
       } else if (message == "!stop") {
-        stopFishing();
+        engine.stopFishing();
       } else if (message == "!eat") {
         setTimeout( () => {
-        eat();
+          engine.eat();
         },500);
       } else if (message.includes("!follow")) {
         const msg = message.split(" ");
         if (msg && msg.length >= 2) {
           const playerToFollow = msg[1].trim("");
-          followPlayer(playerToFollow);
+          engine.followPlayer(playerToFollow);
         }
       } else if (message == "!stop follow") {
-        stopFollowingPlayer();
+        engine.stopFollowingPlayer();
       } else if (message == "!find water") {
-        const waterBlock = await checkForWaterNearby();
-        if (waterBlock) {
-          await bot.lookAt(waterBlock.position.offset(0.5, 0.5, 0.5), true)
-        }
-
+        engine.checkForWaterNearby();
       } else if (message == "!show inventory") {
         const items = getInventory();
         items.forEach( (x) => {
@@ -135,47 +123,8 @@ async function initBot(auth,host, port,username,version) {
 
 }
 
-
 function getInventory() {
   return bot.inventory.slots.filter( (x) => x != null);
-}
-
-function stopFollowingPlayer() {
-  win.webContents.send("game-logs", getLogTime() + " Stop following player");
-  bot.pathfinder.stop();
-  bot.pathfinder.setGoal(null);
-}
-
-function followPlayer(playerName) {
-  if (!bot) {
-    console.log("no bot");
-    return;
-  }
-
-  if (!playerName) {
-    console.log("no player specified");
-    return;
-  }
-
-  const pathfinder = require('mineflayer-pathfinder').pathfinder;
-  bot.loadPlugin(pathfinder);
-
-  const mcData = require('minecraft-data')(bot.version);
-
-  if (isFishing) {
-    stopFishing();
-  }
-  const playerEntity = bot.nearestEntity( (e) => e.type == "player" && e.username == playerName);
-
-  if (!playerEntity) {
-    win.webContents.send("game-logs"," No player to follow found!");
-    return;
-  }
-
-
-  win.webContents.send("game-logs"," Following Player " + playerName);
-  bot.pathfinder.setMovements(new Movements(bot, mcData));
-  bot.pathfinder.setGoal(new GoalFollow(playerEntity, 1),true);
 }
 
 
@@ -197,13 +146,13 @@ async function createWindow() {
   });
 
 
-  ipcMain.handle("follow-player",async(_,name) => {
-    followPlayer(name);
+  ipcMain.handle("follow-player",(_,name) => {
+    engine.followPlayer(name);
   });
 
 
   ipcMain.handle("find-water",async(_) => {
-    await lookAtWater();
+    engine.checkForWaterNearby();
   });
 
   ipcMain.handle("stop-bot",(_) => {
@@ -231,24 +180,23 @@ async function createWindow() {
   })
 
   ipcMain.handle("stop-fishing",async(_) => {
-      console.log("stop fishing!");
-      stopFishing();
+    engine.stopFishing();
   })
 
   ipcMain.handle("stop-following",async(_) => {
     console.log("stop following player!");
-    stopFollowingPlayer();
+    engine.stopFollowingPlayer();
   })
 
   ipcMain.handle("start-fishing",(_) => {
     setTimeout( () => {
       console.log("start fishing!");
-      startFishing();
+      engine.startFishing();
     },1000);
   })
 
   ipcMain.handle("start-bot",async(_,host,port,version,auth,username) => {
-    win.webContents.send("game-logs",getLogTime() + " Starting Bot...");
+    win.webContents.send("game-logs",engine.getLogTime() + " Starting Bot...");
     await initBot(auth,host,port,username,version);
   });
 
@@ -276,146 +224,6 @@ async function createWindow() {
     win.webContents.openDevTools();
   } else {
     win.loadFile(path.join(__dirname, "dist/bob-fisher/browser/index.html"));
-  }
-}
-
-function stopFishing() {
-  win.webContents.send("game-logs", getLogTime() + " Stop Fishing");
-  bot.removeListener("playerCollect",onCollect);
-  if (isFishing) {
-    bot.activateItem();
-    isFishing = false;
-  }
-
-}
-
-
-function checkForFishingRoadInInventory() {
-  if (!bot) { return; }
-
-  const items = bot.inventory.slots.filter( (x) => x != null);
-  const hasRod = items.some( (x) => x.name == "fishing_rod");
-  if (!hasRod) {
-    win.webContents.send("game-logs",getLogTime() + " No fishing rod in my inventory!");
-    console.log("I don't have an fishing rod in my inventory");
-  }
-}
-
-
-async function startFishing() {
-    if (isFishing) {
-      return;
-    }
-
-    const waterBlock = await checkForWaterNearby();
-    if (waterBlock) {
-      await bot.lookAt(waterBlock.position.offset(0.5, 2, 0.5), true)
-    } else {
-      win.webContents.send("game-logs",getLogTime() + " I found no water to fish in!");
-      return;
-    }
-
-
-  if (bot.food < 20) {
-    win.webContents.send("game-logs",getLogTime() + " Eating");
-    await eat();
-  }
-
-
-  try {
-    await bot.equip(bot.registry.itemsByName.fishing_rod.id, 'hand')
-  } catch (err) {
-    console.log(err.message)
-  }
-
-  isFishing = true;
-  bot.on("playerCollect",onCollect);
-
-  try {
-    await bot.fish();
-    console.log("fish!");
-  } catch (err) {
-    console.log(err);
-  }
-  isFishing = false;
-}
-
-
-async function onCollect(player,entity) {
-  if (player !== bot.entity) {
-    return;
-  }
-
-  if (entity.type == "orb" || entity.name == 'experience_orb') {
-    return;
-  }
-
-  bot.removeListener("playerCollect", onCollect);
-
-  const slots = bot.inventory.slots.filter( x => x != null);
-  const itemId = entity.metadata.at(-1).itemId;
-  const item = slots.find(x => x.type == itemId);
-
-  if (!item) {
-    win.webContents.send("game-log", getLogTime() + " I was not able to get the collected item!");
-    return;
-  }
-
-  const lootObj = {
-    name: item.name,
-    displayName: item.displayName,
-    count: item.count,
-    img: null
-  }
-  win.webContents.send("loot-log", lootObj);
-  setTimeout( () => {
-    startFishing();
-  },500);
-}
-
-async function checkForWaterNearby() {
-  try {
-    const waterBlock = await bot.findBlock({
-      point: bot.entity.position,
-      matching: (block) => block.name === 'water',
-      maxDistance: 10
-    })
-    if (!waterBlock) {
-      return false;
-    }
-    return waterBlock;
-
-  } catch (err) {
-    console.log(err);
-  }
-}
-
-
-
-
-
-async function eat () {
-  stopFishing()
-
-  const slots = bot.inventory.slots;
-  const items = slots.filter( (x) => x != null && bot.registry.foodsByName[x.name]);
-  console.log(items.map((x) => x.name));
-
-  if (items.length == 0) {
-    console.log("No food in my inventory.");
-    return;
-  }
-
-  try {
-    await bot.equip(items[0], 'hand')
-  } catch (err) {
-    return console.log(err.message)
-  }
-
-  try {
-    await bot.consume()
-  } catch (err) {
-    console.log(err.message)
   }
 }
 
